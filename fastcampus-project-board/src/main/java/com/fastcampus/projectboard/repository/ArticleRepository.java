7ce72ffbd9d8f0e05e857698362f33df460a4f1e
basilry <basilry@gmail.com>
2023-01-31 18:48:38 +0900

    충돌픽스

--- fastcampus-project-board/src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
+++ fastcampus-project-board/src/main/java/com/fastcampus/projectboard/repository/ArticleRepository.java
@@ -6,13 +6,5 @@ import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 @RepositoryRestResource
 public interface ArticleRepository extends JpaRepository<Article, Long> {
-<<<<<<< HEAD
-<<<<<<< Updated upstream
-}
-=======
 }
 
->>>>>>> Stashed changes
-=======
-}
->>>>>>> master
